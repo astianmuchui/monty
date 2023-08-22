87aff8f834fe8f01264ff74baf9051eb507ada1f
astianmuchui <astianmuchui@gmail.com>
2023-08-22 18:42:49 +0300

    file to write quick tests in

---
+++ testcase.c
@@ -0,0 +1,10 @@
+#include "monty.h"
+
+/**
+* main - test case runner
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: always 0
+*
+* There wont always be a main function here :)
+*/
